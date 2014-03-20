e57927aa74e9d553e841d3d7a1aa61f4a650f27f
matthewia94 <mia2n4@mst.edu>
2014-03-20 15:22:54 -0500

    added chess and final part of the puzzle project

---
+++ chess/Chess-Visualizer--Linux/plugins/chess/textures.r
@@ -0,0 +1,15 @@
+font DroidSansMono textures/DroidSansMono.png DroidSansMono.dat
+font LindenHill textures/LindenHill.png LindenHill.dat
+texture 0-K textures/black-king.png
+texture 0-Q textures/black-queen.png
+texture 0-R textures/black-rook.png
+texture 0-B textures/black-bishop.png
+texture 0-P textures/black-pawn.png
+texture 0-N textures/black-knight.png
+texture 1-K textures/white-king.png
+texture 1-Q textures/white-queen.png
+texture 1-R textures/white-rook.png
+texture 1-B textures/white-bishop.png
+texture 1-P textures/white-pawn.png
+texture 1-N textures/white-knight.png
+texture arrow textures/arrow.png
